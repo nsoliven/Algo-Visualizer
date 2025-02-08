1d99a5049981f42bb86750f5bd6215c0955e0d78
Nevryk Soliven <solivn1@unlv.nevada.edu>
2025-02-07 16:50:29 -0800

    feat: created initial footer, as well as basic card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import  { faCode } from "@fortawesome/free-solid-svg-icons";
 import Header from "../components/header";
+import Footer from "../components/footer";
+import CardStyle1 from "@/components/card";
 
 export default function Home() {
   return (
-    <div>
-      <FontAwesomeIcon icon={faCode}/>
-      <Header/>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow flex flex-col">
+        <h1>Algo-Visualizer</h1>
+        <CardStyle1 card_name="Example Card" card_text="This is a card description." />
+      </main>
+      <Footer />
     </div>
   );
 }
